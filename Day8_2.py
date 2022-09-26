af22b4038a3bf77e44520317fef71e69814c33e6
Qudus Adeyemi <adeyemiqudus361@gmail.com>
2022-09-26 18:47:25 +0100

    prime_checker code

--- Day8_2.py
+++ Day8_2.py
@@ -1,4 +1,13 @@
 #Write your code below this line 👇
+def prime_checker(number):
+    is_prime=True
+    for i in range(2,number):
+        if number % i == 0:
+            is_prime = False
+    if is_prime:
+        print("it's a prime number") 
+    else:
+        print("it's not a prime number")
 
 
 
