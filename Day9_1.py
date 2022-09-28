662dba307ade9463393e4ad608f5e1223bcb9a8b
Qudus Adeyemi <adeyemiqudus361@gmail.com>
2022-09-28 11:09:29 +0100

    Grading program

---
+++ Day9_1.py
@@ -0,0 +1,28 @@
+student_scores = {
+  "Harry": 81,
+  "Ron": 78,
+  "Hermione": 99,
+  "Draco": 74,
+  "Neville": 62,
+}
+# 🚨 Don't change the code above 👆
+
+#TODO-1: Create an empty dictionary called student_grades.
+student_grades = {}
+
+#TODO-2: Write your code below to add the grades to student_grades.👇
+for key,value in student_scores.items():
+
+    if value < 71:
+        student_grades[key] = "Fail"
+    elif value < 81:
+        student_grades[key] = "Acceptable"
+    elif value < 91:
+        student_grades[key] = "Exceeds Expectations"
+    elif value < 101:
+        student_grades[key] = "Outstanding"
+
+
+
+# 🚨 Don't change the code below 👇
+print(student_grades)
